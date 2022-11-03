0ed609241463b434f6f8701351e13caebe474fbb
Nicholas <77742083+NicholasRM@users.noreply.github.com>
2022-11-03 16:11:09 -0400

    add bitpack back

---
+++ bitpack/target/debug/deps/bitpack-524ad5dce6f60a9d.d
@@ -0,0 +1,6 @@
+/mnt/c/VSCode/arith/bitpack/target/debug/deps/bitpack-524ad5dce6f60a9d.rmeta: src/lib.rs src/bitpack.rs
+
+/mnt/c/VSCode/arith/bitpack/target/debug/deps/bitpack-524ad5dce6f60a9d.d: src/lib.rs src/bitpack.rs
+
+src/lib.rs:
+src/bitpack.rs:
